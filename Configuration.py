646c476eb2236ccb1fd5cddc626175a6df848211
555kaireffert@gmail.com <555kaireffert@gmail.com>
2022-11-01 10:28:41 +0100

    better readability

--- Configuration.py
+++ Configuration.py
@@ -4,16 +4,16 @@ from gluonts.mx.distribution import NegativeBinomialOutput
 
 class Configuration:
     def __init__(self):
+        #Time parameter
         self.train_start_time=datetime(2010,1,1,0,0,0)
         self.train_end_time=datetime(2017,12,31,23,0,0)
         self.test_end_time=datetime(2019,12,31,23,0,0)
         self.target='value'
-        #self.target_location="LK Bad Dürkheim"
         
         #deepAR parameters
         self.freq="W-SUN"
         self.context_length=52#number of weeks
-        self.prediction_length=26#number of weeks -> Vielfache:1 Woche (104 TF), 13 Wochen (8 test Frames),26 Wochen (4 TF),52W(2TF),104W(1TF)
+        self.prediction_length=26#number of weeks -> Vielfache:1 Woche (104 Test Windows), 13 Wochen (8 TW),26 Wochen (4 TW),52W(2TW),104W(1TW)
         self.windows=int(104/self.prediction_length)
         self.num_layers=2
         self.num_cells=128
