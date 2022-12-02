aa50f021b66cf81527e74d08cd27e7e3f47a93af
Kai Reffert <555kaireffert@gmail.com>
2022-12-02 10:43:32 +0100

    R setup

---
+++ R/surveillance_implementation.R
@@ -0,0 +1,4 @@
+# setwd("C:/Users/555ka/Coding/GIT-Projects/DeepAR_InfluenzaForecast/DeepAR_InfluenzaForecast/R")
+
+data <- read.csv("C:/Users/555ka/Coding/GIT-Projects/DeepAR_InfluenzaForecast/DeepAR_InfluenzaForecast/Influenza.csv")
+
